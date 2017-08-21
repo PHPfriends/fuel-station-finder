90787e66e454e3fbfcccd0d9b5c73f9c47d9870e
Petar <petargeorgiev123@gmail.com>
2017-08-21 16:52:01 +0200

    Search EndPoint

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,11 +13,31 @@ use FOS\RestBundle\View\View;
 class DefaultController extends FOSRestController
 {
     /**
-     * @Rest\Get("/")
+     * @Rest\Get("/search/{fuel}/{latitud}/{longitud}")
      */
-    public function getAction()
+    public function getAction($fuel, $latitud, $longitud)
     {
-        $restresult = array ('id' => '123');
-        return $restresult;
+        //$latitud = '42,846028';
+        //$longitud = '-2,509361';
+        //http://fuel.dev/app_dev.php/search/G95/42,846028/-2,509361
+
+        $root = dirname(__FILE__)."/../../../Fuel/$fuel.json";
+        $jsonitem = file_get_contents($root);
+        $objitems = json_decode($jsonitem);
+
+
+        $findBy = function($latitud, $longitud) use ($objitems) {
+            foreach ($objitems as $friend) {
+                if ($friend->latitud == $latitud && $friend->longitud == $longitud){
+                    return $friend;
+                }
+            }
+
+            return false;
+        };
+
+        $result = get_object_vars($findBy($latitud, $longitud));
+
+        return $result;
     }
 }
