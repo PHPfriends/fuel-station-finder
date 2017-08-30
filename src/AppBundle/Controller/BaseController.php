ce8a3abbdea8b40a4d7607f3e40868dd1b43e87f
Petar <petargeorgiev123@gmail.com>
2017-08-30 08:40:14 +0200

    refactor a bit

---
+++ src/AppBundle/Controller/BaseController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\View\View;
+
+class BaseController extends FOSRestController
+{
+
+    protected function getUploadDir()
+    {
+        return "Fuel";
+    }
+
+    protected function getUploadedRootDir()
+    {
+        return __DIR__ . '/../../../web/' . $this->getUploadDir();
+    }
+
+    protected function getContent($file)
+    {
+        $content = file_get_contents($file);
+        if ($content === false) {
+            throw new Exception( 'Something really gone wrong');
+        }
+
+        return $content;
+    }
+}
